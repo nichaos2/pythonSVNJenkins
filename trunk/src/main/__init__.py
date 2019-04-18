fa7e205261737e60d32ac18982d631cc526f1588
nichaos2 <nick.ioannou86@gmail.com>
2019-04-18 16:32:56 +0200

    delete test folder

--- trunk/src/main/__init__.py
+++ trunk/src/main/__init__.py
@@ -1,7 +1,7 @@
 def sum(l_arg):
     '''
     add all the arguments in a list
-    simple function to test
+    simple function to tests
     from https://realpython.com/python-testing/
     '''
     total = 0
